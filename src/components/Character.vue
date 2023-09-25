5c7a7d438f23cb0044c72773557c35f3245a16f6
DDMeaqua <lzlloveyou@vip.qq.com>
2023-09-25 22:29:57 +0800

    fix: css

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-24 bg-[#D9EEEC] p-2 rounded-xl shadow-lg m-4">
+  <div class="w-1/4 bg-[#D9EEEC] p-2 rounded-xl shadow-lg sm:m-4 m-3">
     <img src="/hhh.png" alt="" class="rounded-full" />
     <div class="text-center mt-1">xxx</div>
   </div>
