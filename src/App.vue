33af619ea197178c362347bb1429af725192cd2c
Meaqua <lzlloveyou@vip.qq.com>
2023-10-06 21:05:41 +0800

    fix: css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-screen-sm mx-auto min-h-screen bg-fixed bg-[url('./assets/bg.png')] bg-no-repeat bg-center fixed">
+  <div class="max-w-screen-sm mx-auto min-h-screen bg-fixed bg-[url('./assets/bg.png')] bg-no-repeat bg-center">
     <div class="overflow-y-scroll h-screen py-6 lg:py-8">
       <router-view />
     </div>
