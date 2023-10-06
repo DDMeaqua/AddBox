2b607770cfb1b37d7cda5659d2098bd0c832520a
Meaqua <lzlloveyou@vip.qq.com>
2023-10-06 22:03:03 +0800

    feat: 人物

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,21 @@
   <div>
     <div class="px-2">
       <div class="flex flex-wrap">
-        <Character @click="showModal = true" />
-        <Character @click="showModal = true" />
-        <Character @click="showModal = true" />
-        <Character @click="showModal = true" />
-        <Character @click="showModal = true" />
+        <Character
+          v-for="character in characters"
+          :key="character.id"
+          :characterData="character"
+          @click="showModal = true, selectedCharacter = character"
+        />
       </div>
     </div>
 
     <!-- 模态框 -->
-    <div v-show="showModal" class="fixed inset-0 flex items-center justify-center z-10 bg-black/20 backdrop-blur-sm">
+    <div v-if="showModal" class="fixed inset-0 flex items-center justify-center z-10 bg-black/20 backdrop-blur-sm">
       <div class="bg-white w-4/5 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 rounded-xl shadow-lg">
-        <h2 class="text-xl font-bold mb-2 text-center">选择了 xxx</h2>
-        <img src="/hhh.png" alt="" class="rounded-full w-1/3 mx-auto">
-        <p class="text-gray-500 text-sm text-center my-2">作者：xxx ——《书名》</p>
+        <h2 class="text-xl font-bold mb-2 text-center">选择了 {{ selectedCharacter.name }}</h2>
+        <img :src="selectedCharacter.imageUrl" alt="" class="rounded-full w-1/3 mx-auto">
+        <p class="text-gray-500 text-sm text-center my-4">作者：{{ selectedCharacter.author }} ——《{{ selectedCharacter.bookName }}》</p>
         <div class="text-center">
         <!-- 前往按钮，点击后导航到新页面 -->
         <router-link to="/main">
@@ -36,8 +37,26 @@
 
 <script setup>
 import Character from "../components/Character.vue";
-
 import { ref } from 'vue';
 
 const showModal = ref(false);
+const selectedCharacter = ref(null);
+
+// 人物卡片数据数组
+const characters = [
+  {
+    id: 1,
+    name: "许晶晶",
+    imageUrl: "/xujingjing.png",
+    author: "阎真",
+    bookName: "如何是好",
+  },
+  {
+    id: 2,
+    name: "叶文洁",
+    imageUrl: "/yewenjie.png",
+    author: "刘慈欣",
+    bookName: "三体",
+  },
+];
 </script>
